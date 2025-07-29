fa3a514851a31a9d0d988f12d19790255d44ef1a
vilasini <115165428+vilasini-v@users.noreply.github.com>
2025-07-29 18:59:33 +0530

    resume update :3

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import StarRoundedIcon from "@mui/icons-material/StarRounded";
 
 export function Navbar({ color }) {
   const handleOpenResume = () => {
-    window.open("https://drive.google.com/drive/folders/1XAJwl3lsNg5jHcKFkdcKURTDEJzQSQub?usp=sharing", "_blank");
+    window.open("https://drive.google.com/file/d/1THJH3_batzQbNdDgIGb570bBlMC8LTFk/view?usp=sharing", "_blank");
   };  
   return (
     <div
